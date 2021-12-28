1882de304cdc08fc1120a50d359f6203ba8da5c5
Rudolf <spureszka@gmail.com>
2021-12-28 17:17:19 +0100

    create Header and Footer components

---
+++ client/src/routes/components/header.js
@@ -0,0 +1,5 @@
+export default function Header(){
+    return (
+        <p>Its header</p>
+    )
+}
--- client/src/routes/pages/home.js
+++ client/src/routes/pages/home.js
@@ -1,5 +1,16 @@
 import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function Home() {
-  return <h1>Hello React</h1>;
+  return (
+    <div>
+      <Header />
+      <div>
+        <h1>Home</h1>
+        <h3>It's React Home route</h3>
+      </div>
+      <Footer />
+    </div>
+  );
 }
