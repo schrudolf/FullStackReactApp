f5bbfa0dbf08c412a233a1bf59db9899983dc0ce
Rudolf <spureszka@gmail.com>
2022-01-01 13:12:09 +0100

    check exists email before register

--- server/src/middlewares/register/checkIncomingRegisterData.ts
+++ server/src/middlewares/register/checkIncomingRegisterData.ts
@@ -1,32 +1,41 @@
 import { Request, Response, NextFunction } from 'express';
 
-export default function checkIncomingRegisterData(db: any){
-    return function(req: Request, res: Response, next: NextFunction){
-        const {email, password, password2} = req.body;
+export default function checkIncomingRegisterData(db: any) {
+    return async function (req: Request, res: Response, next: NextFunction) {
+        const { email, password, password2 } = req.body;
 
-        if(typeof email === "undefined" || typeof password === "undefined" || typeof password2 === "undefined"){
-            return res.status(204).end(); 
-        }
-        if(email === "" || password === "" || password2 === ""){
+        if (typeof email === "undefined" || typeof password === "undefined" || typeof password2 === "undefined") {
             return res.status(204).end();
         }
-        if(!email || !password || !password2){
+        if (email === "" || password === "" || password2 === "") {
+            return res.status(204).end();
+        }
+        if (!email || !password || !password2) {
             return res.status(204).end()
         }
-        if(password !== password2){
-            return res.status(200).send({msg: "Passwords do not match"})
+        if (password !== password2) {
+            return res.status(200).send({ msg: "Passwords do not match" })
         }
-        if(password.length < 5){
-            return res.status(200).send({msg: "Password is too short"})
+        if (password.length < 5) {
+            return res.status(200).send({ msg: "Password is too short" })
         }
-        if(password.length > 15){
-            return res.status(200).send({msg: "Password is too long"})
+        if (password.length > 15) {
+            return res.status(200).send({ msg: "Password is too long" })
         }
-        res.locals.newUser = {
-            email,
-            password,
-            password2
+        try {
+            const checkEmail = await db.query("SELECT * FROM users WHERE email=?", [email]);
+            if(checkEmail[0].length > 0){
+                return res.status(200).send({ msg: "This email already exists" })
+            }else{
+                res.locals.newUser = {
+                    email,
+                    password
+                }
+                next()
+            }
         }
-        next()
+        catch (err) {
+            next(err);
+        }
     }
 }
