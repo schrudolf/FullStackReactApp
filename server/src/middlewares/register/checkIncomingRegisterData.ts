d79b77bbfcba99106b4406a6e289b074d61366dd
Rudolf <spureszka@gmail.com>
2022-01-01 12:58:17 +0100

    create checkIncomingRegisterData middleware

--- server/src/middlewares/register/checkIncomingRegisterData.ts
+++ server/src/middlewares/register/checkIncomingRegisterData.ts
@@ -2,7 +2,31 @@ import { Request, Response, NextFunction } from 'express';
 
 export default function checkIncomingRegisterData(db: any){
     return function(req: Request, res: Response, next: NextFunction){
-        console.log(req.body);
-        res.status(200).send("Success registration")
+        const {email, password, password2} = req.body;
+
+        if(typeof email === "undefined" || typeof password === "undefined" || typeof password2 === "undefined"){
+            return res.status(204).end(); 
+        }
+        if(email === "" || password === "" || password2 === ""){
+            return res.status(204).end();
+        }
+        if(!email || !password || !password2){
+            return res.status(204).end()
+        }
+        if(password !== password2){
+            return res.status(200).send({msg: "Passwords do not match"})
+        }
+        if(password.length < 5){
+            return res.status(200).send({msg: "Password is too short"})
+        }
+        if(password.length > 15){
+            return res.status(200).send({msg: "Password is too long"})
+        }
+        res.locals.newUser = {
+            email,
+            password,
+            password2
+        }
+        next()
     }
 }
