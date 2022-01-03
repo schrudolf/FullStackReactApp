b3c3b1774b5b96affc6757b15d14dc8da03284b4
Rudolf <spureszka@gmail.com>
2022-01-03 13:21:14 +0100

    implementing login system on the client side

--- client/src/routes/pages/home/login.js
+++ client/src/routes/pages/home/login.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -6,12 +7,46 @@ import Button from "@mui/material/Button";
 import Link from "@mui/material/Link";
 import IconButton from "@mui/material/IconButton";
 import EmailIcon from "@mui/icons-material/Email";
-import LockIcon from '@mui/icons-material/Lock';
+import LockIcon from "@mui/icons-material/Lock";
 
 import Header from "../../../components/home/header";
 import Footer from "../../../components/home/footer";
 
+import LoadingButton from "../../../components/ui/loadingButton";
+import createNewAxios from "../../../axios/axios";
+import AC from "../../../redux/action-creators/bindActionCreators";
+
 export default function Login() {
+  const [loadingButton, setLoadingButton] = useState(false);
+  const actionCreators = AC();
+
+  async function userLogin(e) {
+    e.preventDefault();
+    setLoadingButton(true);
+
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const response_msg = document.getElementById("response_msg");
+
+    const response = await createNewAxios("/login", "post", {
+      email: email.value,
+      password: password.value,
+    });
+
+    response_msg.innerHTML = "";
+    setLoadingButton(false);
+    if (response.status === 200 && response.data.success) {
+      email.value = "";
+      password.value = "";
+      response_msg.style.color = "green";
+      response_msg.innerHTML = response.data.msg;
+      // set isLogged value === true and redirect to protected route
+      actionCreators.setLoggedStatus(true);
+    } else {
+      response_msg.style.color = "red";
+      response_msg.innerHTML = response.data.msg;
+    }
+  }
   return (
     <div>
       <Header />
@@ -30,10 +65,15 @@ export default function Login() {
           }}
         >
           <Box>
-          <Typography m={5} component="h1" variant="h5" sx={{ fontWeight: 'bold' }}>
-            Log in
-          </Typography>
-            <form>
+            <Typography
+              m={5}
+              component="h1"
+              variant="h5"
+              sx={{ fontWeight: "bold" }}
+            >
+              Log in
+            </Typography>
+            <form onSubmit={userLogin}>
               <TextField
                 margin="normal"
                 required
@@ -56,10 +96,6 @@ export default function Login() {
                   borderRadius: 1,
                 }}
               />
-              <p
-                style={{ margin: 0, textAlign: "center" }}
-                id="emailAnswer"
-              ></p>
               <TextField
                 margin="normal"
                 required
@@ -69,24 +105,34 @@ export default function Login() {
                 type="password"
                 placeholder="Password"
                 InputProps={{
-                    startAdornment: (
-                      <IconButton tabIndex={-1}>
-                        <LockIcon />
-                      </IconButton>
-                    ),
-                  }}
+                  startAdornment: (
+                    <IconButton tabIndex={-1}>
+                      <LockIcon />
+                    </IconButton>
+                  ),
+                }}
                 sx={{
                   backgroundColor: "white",
                   borderRadius: 1,
                 }}
               />
               <p
-                style={{ margin: 0, textAlign: "center" }}
-                id="passwordAnswer"
+                style={{ margin: 10, textAlign: "center" }}
+                id="response_msg"
               ></p>
-              <Button type="submit" size="large" fullWidth variant="contained" color="info">
-                Log in
-              </Button>
+              {loadingButton ? (
+                <LoadingButton />
+              ) : (
+                <Button
+                  type="submit"
+                  size="large"
+                  fullWidth
+                  variant="contained"
+                  color="info"
+                >
+                  Sign up
+                </Button>
+              )}
               <div style={{ margin: 5 }}>
                 <span>or </span>
                 <Link href="/forgot" variant="body2">
