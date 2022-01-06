1115cb0a6425e7b8b7b0d21cae434562407c181b
Rudolf <spureszka@gmail.com>
2022-01-07 00:34:51 +0100

    someFix for newPassword page

--- client/src/routes/pages/home/newPassword.js
+++ client/src/routes/pages/home/newPassword.js
@@ -1,28 +1,27 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 // import LoadingButton from "../../../components/ui/loadingButton";
 import createNewAxios from "../../../axios/axios";
 
 export default function NewPassword() {
-  const [validToken, setValidToken] = useState(false);
+  // const [loadingButton, setLoadingButton] = useState(false);
+  const [responseStatus, setResponseStatus] = useState();
   // get the tokenid from route params
   const { tokenid } = useParams();
-  // const [loadingButton, setLoadingButton] = useState(false);
+
   const tokenCheck = async () => {
     const response = await createNewAxios("/forgot/" + tokenid, "get");
-    if (response.status === 200) {
-      setValidToken(true);
-    } else {
-      console.log("Not valid or exists");
-    }
+    setResponseStatus(response.status);
   };
+
   useEffect(() => {
     tokenCheck();
-  });
-
-  if (!validToken) {
-    return <h1>Token is not valid</h1>;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
+  if (responseStatus > 200) {
+    return <Navigate to="/forgot" />;
   } else {
-    return <h1>Token is valid</h1>;
+    return <h1>Valid token</h1>
   }
 }
