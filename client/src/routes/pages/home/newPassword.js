989159b8849dc78533d2cf9e414fd760b0093a15
Rudolf <spureszka@gmail.com>
2022-01-08 18:56:56 +0100

    rendering newPassword component if valid token or not expired

--- client/src/routes/pages/home/newPassword.js
+++ client/src/routes/pages/home/newPassword.js
@@ -1,11 +1,20 @@
 import { useState, useEffect } from "react";
 import { useParams, Navigate } from "react-router-dom";
-// import LoadingButton from "../../../components/ui/loadingButton";
+import {Container, Box, Typography, TextField, IconButton, Button} from "@mui/material";
+import LockIcon from "@mui/icons-material/Lock";
+
+import PageLoading from "../../../components/ui/pageLoading";
+import LoadingButton from "../../../components/ui/loadingButton";
+
 import createNewAxios from "../../../axios/axios";
 
+import Header from "../../../components/home/header";
+import Footer from "../../../components/home/footer";
+
+// if valid link and token not expired render the newPassword component (status 204 not valid or expired. status 200 volid token)
 export default function NewPassword() {
-  // const [loadingButton, setLoadingButton] = useState(false);
-  const [responseStatus, setResponseStatus] = useState();
+  const [loadingButton, setLoadingButton] = useState(false);
+  const [responseStatus, setResponseStatus] = useState({});
   // get the tokenid from route params
   const { tokenid } = useParams();
 
@@ -18,10 +27,130 @@ export default function NewPassword() {
     tokenCheck();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  
-  if (responseStatus > 200) {
-    return <Navigate to="/forgot" />;
+
+  async function sendNewPassword(e) {
+    e.preventDefault();
+    setLoadingButton(true);
+
+    const password = document.getElementById("password");
+    const password2 = document.getElementById("password2");
+    const response_msg = document.getElementById("response_msg");
+
+    const response = await createNewAxios(`/forgot/${tokenid}/newpassword`, "post", {
+      password: password.value,
+      password2: password2.value,
+    });
+
+    response_msg.innerHTML = "";
+    setLoadingButton(false);
+    if (response.status === 200 && response.data.success) {
+      password.value = "";
+      password2.value = "";
+      response_msg.style.color = "green";
+      response_msg.innerHTML = response.data.msg;
+      // set isLogged value === true and redirect to protected route
+    } else {
+      response_msg.style.color = "red";
+      response_msg.innerHTML = response.data.msg;
+    }
+  }
+  if (responseStatus >= 200) {
+    if (responseStatus > 200) {
+      return <Navigate to="/forgot" />;
+    } else {
+      return (
+        <div>
+          <Header />
+          <div style={{ padding: 3 }}>
+            <Container
+              maxWidth="xs"
+              sx={{
+                p: 1,
+                display: {
+                  position: "relative",
+                  top: "15vh",
+                  textAlign: "center",
+                  backgroundColor: "#dce1e3",
+                  borderRadius: 10,
+                },
+              }}
+            >
+              <Box>
+                <Typography
+                  m={5}
+                  component="h1"
+                  variant="h5"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  New Password
+                </Typography>
+                <form onSubmit={sendNewPassword}>
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    id="password"
+                    type="password"
+                    placeholder="Password"
+                    InputProps={{
+                      startAdornment: (
+                        <IconButton tabIndex={-1}>
+                          <LockIcon />
+                        </IconButton>
+                      ),
+                    }}
+                    sx={{
+                      backgroundColor: "white",
+                      borderRadius: 1,
+                    }}
+                  />
+                  <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password2"
+                    id="password2"
+                    type="password"
+                    placeholder="Password again"
+                    InputProps={{
+                      startAdornment: (
+                        <IconButton tabIndex={-1}>
+                          <LockIcon />
+                        </IconButton>
+                      ),
+                    }}
+                    sx={{
+                      backgroundColor: "white",
+                      borderRadius: 1,
+                    }}
+                  />
+                  <p
+                    style={{ margin: 10, textAlign: "center" }}
+                    id="response_msg"
+                  ></p>
+                  {loadingButton ? (
+                    <LoadingButton />
+                  ) : (
+                    <Button
+                      type="submit"
+                      size="large"
+                      fullWidth
+                      variant="contained"
+                      color="info"
+                    >
+                      Confirm
+                    </Button>
+                  )}
+                </form>
+              </Box>
+            </Container>
+          </div>
+          <Footer />
+        </div>
+      );
+    }
   } else {
-    return <h1>Valid token</h1>
+    return <PageLoading />;
   }
 }
