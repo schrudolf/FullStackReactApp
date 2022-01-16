a153fca2dcd9f3ac1852bc4f544390b1dba9ac6d
Rudolf <spureszka@gmail.com>
2022-01-17 00:56:29 +0100

    some fix in user navigation

--- client/src/routes/pages/app/settings/account.js
+++ client/src/routes/pages/app/settings/account.js
@@ -11,18 +11,10 @@ export default function AccountSettings() {
       <AppHeader />
       <Container maxWidth="lg">
         <Grid container>
-          <Grid item textAlign={"left"} xs={8}>
+          <Grid item textAlign={"left"} xs={12} sm={8}>
             <h1>Account</h1>
           </Grid>
-          <Grid
-            item
-            textAlign={"left"}
-            mt={3}
-            sx={{ border: "1px solid black", borderRadius: 4 }}
-            xs={4}
-          >
-            <NavLinks />
-          </Grid>
+          <NavLinks />
         </Grid>
       </Container>
       <Footer />
--- client/src/routes/pages/app/settings/navLinks.js
+++ client/src/routes/pages/app/settings/navLinks.js
@@ -1,26 +1,25 @@
-import { ListItem } from "@mui/material";
+import { ListItem, Grid } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
-import  "./navLink.css"
+import "./navLink.css";
 
 const activeStyle = ({ isActive }) =>
   isActive
     ? {
         width: "100%",
         color: "black",
-        padding: 12,
+        padding: 15,
         backgroundColor: "#DADADA",
         textDecoration: "none",
-        fontWeight: 800,
-        borderRadius: 10,
+        fontWeight: 600,
+        borderRadius: 15,
       }
     : {
         width: "100%",
         color: "black",
-        padding: 12,
+        padding: 15,
         backgroundColor: "white",
         textDecoration: "none",
-        fontWeight: 600,
       };
 
 const links = [
@@ -42,5 +41,19 @@ const links = [
 ];
 
 export default function NavLinks() {
-  return links.map((item, i) => <ListItem className="link-list" key={i}>{item}</ListItem>);
+  return (
+    <Grid
+      item
+      textAlign={"left"}
+      mt={3}
+      sx={{ border: "2px solid rgb(128, 128, 128, 0.4)", borderRadius: 4, display: {xs: "none", sm: "grid"} }}
+      sm={4}
+    >
+      {links.map((item, i) => (
+        <ListItem className="link-list" key={i}>
+          {item}
+        </ListItem>
+      ))}
+    </Grid>
+  );
 }
