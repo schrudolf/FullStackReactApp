d35473e42d978d7296de4ba1a95f8e5c298db484
Rudolf <spureszka@gmail.com>
2022-01-31 23:57:43 +0100

    error handler middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,6 +25,10 @@ async function startServer() {
     }))
     const con = await db();
     routes(app, con);
+    app.use(function (err: any, req: any, res: any, next: any) {
+        console.error(err.stack)
+        res.status(500).send('Something broke!')
+    })
     app.listen(parseInt(PORT), HOST, () => {
         console.log(`Server is listening on http://${HOST}:${PORT}`);
     });
