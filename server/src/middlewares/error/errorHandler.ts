59c73a289bdd307a0d3f01e444f718a7541da366
Rudolf <spureszka@gmail.com>
2022-02-02 01:17:20 +0100

    saved errors to src/logs/error.log

--- server/src/middlewares/error/errorHandler.ts
+++ server/src/middlewares/error/errorHandler.ts
@@ -1,10 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
+import path from 'path';
+import fs from "fs/promises";
 
 export default function errorHandler() {
-    return async function (err: any, req: Request, res: Response, next: NextFunction) {
+    return async function (err: Error, req: Request, res: Response, next: NextFunction) {
         try {
-            console.error(err.stack)
-            res.status(500).send('Something broke!')
+            const dir = path.join(__dirname, "../../logs")
+            await fs.mkdir(dir, { recursive: true });
+            const date = new Date().toLocaleString();
+            await fs.appendFile(dir + "/error.log", `\r\n \r\n>>>> ${err.name}: ${date} <<<<<< \r\n \r\n ${err.message} \r\n ${err.stack} `)
+            console.log("An ERROR -->> saved to src/logs/error.log file")
+            next();
         }
         catch (err) {
             next(err);
