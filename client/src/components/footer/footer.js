b24880a01a544770d2848e94ce6807c96d998614
Rudolf <spureszka@gmail.com>
2022-03-08 12:44:40 +0100

    update footer.js

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -1,83 +1,86 @@
 import * as React from "react";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography"
+import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import { Box } from "@mui/system";
 
-import "./footer.css"
+import "./footer.css";
 
 const date = new Date();
 
 export default function Footer() {
   return (
     <footer className="footer">
-        <Container maxWidth="lg">
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Content One</Box>
-              <Box>
-                <Link href="/" color="inherit">
-                  Link One
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
+      <Container maxWidth="lg">
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={4}>
+            <Box borderBottom={1}>Content One</Box>
+            <Box>
+              <Link href="/" color="inherit">
+                Link One
+              </Link>
+            </Box>
+            <Box>
+              <Link href="/" color="inherit">
                 Link Two
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
+              </Link>
+            </Box>
+            <Box>
+              <Link href="/" color="inherit">
                 Link Three
-                </Link>
-              </Box>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Content Two</Box>
-              <Box>
-                <Link href="/" color="inherit">
-                  Link One
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
-                Link Two
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
-                Link Three
-                </Link>
-              </Box>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Content Three</Box>
-              <Box>
-                <Link href="/" color="inherit">
-                  Link One
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
-                Link Two
-                </Link>
-              </Box>
-              <Box>
-                <Link href="/" color="inherit">
-                Link Three
-                </Link>
-              </Box>
-            </Grid>
+              </Link>
+            </Box>
           </Grid>
-          <Box textAlign="center" pt={{ xs: 1, sm: 3 }} pb={{ xs: 3, sm: 0 }}>
+          <Grid item xs={12} sm={4}>
+            <Box borderBottom={1}>Content Two</Box>
+            <Box>
+              <Link href="/" color="inherit">
+                Link One
+              </Link>
+            </Box>
+            <Box>
+              <Link href="/" color="inherit">
+                Link Two
+              </Link>
+            </Box>
+            <Box>
+              <Link href="/" color="inherit">
+                Link Three
+              </Link>
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Box borderBottom={1}>Content Three</Box>
+            <Box>
+              <Link href="/" color="inherit">
+                Link One
+              </Link>
+            </Box>
+            <Box>
+              <Link href="/" color="inherit">
+                Link Two
+              </Link>
+            </Box>
+            <Box>
+              <Link href="/" color="inherit">
+                Link Three
+              </Link>
+            </Box>
+          </Grid>
+        </Grid>
+        <Box textAlign="center" pt={{ xs: 1, sm: 3 }} pb={{ xs: 3, sm: 0 }}>
           <Typography variant="body1" margin={"auto"} color="inherit">
-              FullStackReactApp
-           </Typography>
-          </Box>
-          <Box textAlign="center" pt={{ xs: 1, sm: 2 }} pb={{ xs: 2, sm: 0 }}>
-          © {date.getUTCFullYear()} <Link href="https://github.com/schrudolf" color="inherit">schrudolf</Link>
-          </Box>
-        </Container>
+            FullStackReactApp
+          </Typography>
+        </Box>
+        <Box textAlign="center" pt={{ xs: 1, sm: 2 }} pb={{ xs: 2, sm: 0 }}>
+          © {date.getUTCFullYear()}{" "}
+          <Link href="https://github.com/schrudolf" color="inherit">
+            schrudolf
+          </Link>
+        </Box>
+      </Container>
     </footer>
   );
 }
