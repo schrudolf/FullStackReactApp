d520ca507b10c5c96acf2c1ee5061928b05a0b73
Rudolf <spureszka@gmail.com>
2022-03-09 13:17:22 +0100

    update settings navlinks

--- client/src/routes/pages/protected/settings/navLinks.js
+++ client/src/routes/pages/protected/settings/navLinks.js
@@ -3,63 +3,22 @@ import { NavLink } from "react-router-dom";
 
 import "./navLink.css";
 
-const activeStyle = ({ isActive }) =>
-  isActive
-    ? {
-        width: "100%",
-        color: "white",
-        padding: 10,
-        backgroundColor: "black",
-        textDecoration: "none",
-        fontWeight: 600,
-        borderRadius: 10,
-      }
-    : {
-        width: "100%",
-        color: "black",
-        padding: 10,
-        backgroundColor: "white",
-        textDecoration: "none",
-        borderRadius: 10,
-      };
-
 const links = [
-  <NavLink className={"link"} to="/app/settings/profile" style={activeStyle}>
+  <NavLink className="link" to="/app/settings/profile">
     User Profile
   </NavLink>,
-  <NavLink className={"link"} to="/app/settings/details" style={activeStyle}>
+  <NavLink className="link" to="/app/settings/details">
     User Details
   </NavLink>,
-  <NavLink className={"link"} to="/app/settings/password" style={activeStyle}>
+  <NavLink className="link" to="/app/settings/password">
     Password
-  </NavLink>,
-  <NavLink className={"link"} to="/#" style={activeStyle}>
-    Notifications
-  </NavLink>,
-  <NavLink className={"link"} to="/#" style={activeStyle}>
-    Link 2
-  </NavLink>,
-  <NavLink className={"link"} to="/#" style={activeStyle}>
-    Link 3
   </NavLink>,
 ];
 
 export default function NavLinks() {
   return (
-    <Grid
-      item
-      textAlign={"left"}
-      mt={3}
-      sx={{ display: { xs: "none", sm: "block" } }}
-      sm={4}
-    >
-      <div
-        style={{
-          border: "1px solid rgb(128, 128, 128, 0.4)",
-          backgroundColor: "#DADADA",
-          borderRadius: "15px",
-        }}
-      >
+    <Grid item mt={3} sx={{ display: { xs: "none", sm: "block" } }} sm={4}>
+      <div className="settings_nav">
         <Typography
           m={2}
           variant="h5"
